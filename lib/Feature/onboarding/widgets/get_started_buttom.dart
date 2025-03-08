5cad846794bf32f673503a1281d7dcecded165c2
Fares Ayman <fareshe73@gmail.com>
2025-03-08 19:38:19 +0200

    update name text style file

--- lib/Feature/onboarding/widgets/get_started_buttom.dart
+++ lib/Feature/onboarding/widgets/get_started_buttom.dart
@@ -21,7 +21,7 @@ class GetStartedButtom extends StatelessWidget {
           RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
         ),
       ),
-      child: Text('Get Started', style: Styles.font16WhiteSemiBold),
+      child: Text('Get Started', style: Styles.font16WhiteSemibold),
     );
   }
 }
